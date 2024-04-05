e0fc73819b9084b4e5d3de1e448af26d7d9c6a63
Matthew Stibbins <48364517+stibbm@users.noreply.github.com>
2024-04-05 00:52:15 -0700

    implment GetAllBooksPagedActivity

---
+++ src/main/java/book/data/service/activity/GetAllBooksPagedActivity.java
@@ -0,0 +1,55 @@
+package book.data.service.activity;
+
+import static book.data.service.constants.Routes.ALL_ORIGINS;
+import static book.data.service.constants.Routes.GET_ALL_BOOKS_PAGED;
+
+import book.data.service.message.book.GetAllBooksPagedRequest;
+import book.data.service.message.book.GetAllBooksPagedResponse;
+import book.data.service.model.Book;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import book.data.service.manager.BookManager;
+import book.data.service.firebase.FirebaseService;
+
+@Slf4j
+@RestController
+public class GetAllBooksPagedActivity {
+    private BookManager bookManager;
+    private FirebaseService firebaseService;
+
+    @Autowired
+    public GetAllBooksPagedActivity(
+        BookManager bookManager,
+        FirebaseService firebaseService
+    ) {
+        this.bookManager = bookManager;
+        this.firebaseService = firebaseService;
+    }
+
+    @PostMapping(GET_ALL_BOOKS_PAGED)
+    @CrossOrigin(ALL_ORIGINS)
+    public @ResponseBody GetAllBooksPagedResponse getAllBooksPaged(
+        @RequestBody GetAllBooksPagedRequest getAllBooksPagedRequest,
+        @RequestHeader("Authorization") String authToken
+    ) {
+        String createdBy = firebaseService.getEmail(authToken);
+        List<Book> bookList = bookManager.getAllBooksPaged(
+            Integer.parseInt(getAllBooksPagedRequest.getPageNumber()),
+            Integer.parseInt(getAllBooksPagedRequest.getPageSize()),
+            createdBy
+        );
+        GetAllBooksPagedResponse getAllBooksPagedResponse = GetAllBooksPagedResponse.builder()
+            .bookList(bookList)
+            .pageNumber(Integer.parseInt(getAllBooksPagedRequest.getPageNumber()))
+            .pageSize(Integer.parseInt(getAllBooksPagedRequest.getPageSize()))
+            .build();
+        return getAllBooksPagedResponse;
+    }
+}
---
+++ src/test/java/test/book/data/service/activity/book/GetAllBooksPagedActivityTest.java
@@ -0,0 +1,57 @@
+package test.book.data.service.activity.book;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static test.book.data.service.dao.BookDAOTest.BOOK_LIST;
+import static test.book.data.service.dao.BookDAOTest.CREATED_BY_ONE;
+import static test.book.data.service.dao.BookDAOTest.PAGE_NUMBER;
+import static test.book.data.service.dao.BookDAOTest.PAGE_SIZE;
+
+import book.data.service.activity.GetAllBooksPagedActivity;
+import book.data.service.firebase.FirebaseService;
+import book.data.service.manager.BookManager;
+import book.data.service.message.book.GetAllBooksPagedRequest;
+import book.data.service.message.book.GetAllBooksPagedResponse;
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class GetAllBooksPagedActivityTest {
+
+    public static final String AUTH_TOKEN = "ogiheohes";
+    public static final GetAllBooksPagedRequest GET_ALL_BOOKS_PAGED_REQUEST =
+        GetAllBooksPagedRequest.builder().pageNumber("" + PAGE_NUMBER).pageSize("" + PAGE_SIZE)
+            .build();
+    public static final GetAllBooksPagedResponse GET_ALL_BOOKS_PAGED_RESPONSE =
+        GetAllBooksPagedResponse.builder()
+            .bookList(BOOK_LIST)
+            .pageNumber(PAGE_NUMBER)
+            .pageSize(PAGE_SIZE)
+            .build();
+
+    private GetAllBooksPagedActivity getAllBooksPagedActivity;
+
+    @Mock
+    private BookManager bookManager;
+    @Mock
+    private FirebaseService firebaseService;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        this.getAllBooksPagedActivity = new GetAllBooksPagedActivity(bookManager, firebaseService);
+    }
+
+    @Test
+    public void testGetAllBooksPaged() {
+        when(firebaseService.getEmail(anyString())).thenReturn(CREATED_BY_ONE);
+        when(bookManager.getAllBooksPaged(PAGE_NUMBER, PAGE_SIZE, CREATED_BY_ONE))
+            .thenReturn(BOOK_LIST);
+        GetAllBooksPagedResponse getAllBooksPagedResponse =
+            getAllBooksPagedActivity.getAllBooksPaged(GET_ALL_BOOKS_PAGED_REQUEST, AUTH_TOKEN);
+        Assertions.assertThat(getAllBooksPagedResponse).isEqualTo(GET_ALL_BOOKS_PAGED_RESPONSE);
+    }
+
+}
