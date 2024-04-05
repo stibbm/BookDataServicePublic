418db60e8f82996678d0cc3088a485d6815a10b1
Matthew Stibbins <48364517+stibbm@users.noreply.github.com>
2024-04-05 00:39:47 -0700

    add constants Routes

---
+++ src/main/java/book/data/service/constants/Routes.java
@@ -0,0 +1,21 @@
+package book.data.service.constants;
+
+import com.google.gson.Gson;
+
+public class Routes {
+    public static final String CREATE_BOOK = "/createBook";
+    public static final String CREATE_CHAPTER = "/createChapter";
+    public static final String CREATE_IMAGE = "/createImage";
+    public static final String GET_ALL_BOOKS_PAGED = "/getAllBooksPaged";
+    public static final String GET_BOOK_BY_BOOK_NAME = "/getBookByBookName";
+    public static final String GET_CHAPTER_BY_BOOK_NAME_AND_CHAPTER_NUMBER =
+        "/getChapterByBookNameAndChapterNumber";
+    public static final String GET_CHAPTERS_BY_BOOK_NAME_PAGED = "/getChaptersByBookNamePaged";
+    public static final String GET_IMAGES_BY_BOOK_NAME_AND_CHAPTER_NUMBER_PAGED =
+        "/getImagesByBookNameAndChapterNumberPaged";
+    public static final String GET_IMAGE_BY_BOOK_NAME_AND_CHAPTER_NUMBER_AND_IMAGE_NUMBER =
+        "/getImageByBookNameAndChapterAndChapterNumberAndImageNumber";
+
+    public static final String ALL_ORIGINS = "*";
+    public static final Gson GSON = new Gson();
+}
