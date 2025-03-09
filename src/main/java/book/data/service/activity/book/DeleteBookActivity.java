6724ae551392f7d6fa0b720845761e9948765af0
Matthew Stibbins <matthewstibbins@gmail.com>
2025-03-08 22:32:25 -0800

    add DeleteBookActivity

--- src/main/java/book/data/service/activity/book/DeleteBookActivity.java
+++ src/main/java/book/data/service/activity/book/DeleteBookActivity.java
@@ -1,5 +1,29 @@
 package book.data.service.activity.book;
 
+@Slf4j
+@RestController
 public class DeleteBookActivity {
+  private BookManager bookManager;
+  private FirebaseService firebaseService;
 
+  @Autowired
+  public DeleteBookActivity(BookManager bookManager, FirebaseService firebaseService) {
+    this.bookManager = bookManager;
+    this.firebaseService = firebaseService;
+  }
+
+  @PostMapping(DELETE_BOOK_BY_NAME)
+  @CrossOrigin(ALL_ORIGINS)
+  public @ResponseBody DeleteBookResponse deleteBook(
+    @RequestBody DeleteBookRequest deleteBookRequest,
+    @RequestHeader("Authorization") String authToken
+  ) {
+    log.info("Deleting book: " + deleteBookRequest.toString());
+    String createdBy = firebaseService.getEmail(authToken);
+    String deletedBook = bookManager.deleteBook(deleteBookRequest.getBookName(), createdBy);
+    DeleteBookResponse deleteBookResponse = DeleteBookResponse.builder()
+      .book(deletedBook)
+      .build();
+    return deleteBookResponse;
+  }
 }
