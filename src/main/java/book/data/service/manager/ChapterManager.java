2cde54f6f4363b961a38e1a72751842b6920c524
matthew stibbins <matthewstibbins@gmail.com>
2025-03-09 23:45:20 -0400

    Add new chapter management methods to ChapterManager
    Introduced methods for checking chapter existence, retrieving chapters by name, and updating chapter lock status. Enhanced the `createChapter` method to include a timestamp and lock status, utilizing the newly injected TimeService. These changes improve chapter handling and extend functionality.

--- src/main/java/book/data/service/manager/ChapterManager.java
+++ src/main/java/book/data/service/manager/ChapterManager.java
@@ -1,14 +1,16 @@
 package book.data.service.manager;
 
+
+import java.util.List;
+
 import book.data.service.dao.book.BookDAO;
 import book.data.service.dao.chapter.ChapterDAO;
 import book.data.service.exception.BookDoesNotExistException;
 import book.data.service.exception.ChapterAlreadyExistsException;
 import book.data.service.exception.ChapterDoesNotExistException;
-import java.util.List;
-
 import book.data.service.sqlmodel.book.Book;
 import book.data.service.sqlmodel.chapter.Chapter;
+import book.data.service.sqlmodel.chapter.LockStatus;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -18,51 +20,95 @@ import org.springframework.stereotype.Component;
 public class ChapterManager {
     private ChapterDAO chapterDAO;
     private BookDAO bookDAO;
+    private TimeService timeService;
 
     @Autowired
     public ChapterManager(
-        ChapterDAO chapterDAO,
-        BookDAO bookDAO
+            ChapterDAO chapterDAO,
+            BookDAO bookDAO,
+            TimeService timeService
     ) {
         this.chapterDAO = chapterDAO;
         this.bookDAO = bookDAO;
+        this.timeService = timeService;
     }
 
     public List<Chapter> getAllChaptersPaged(int pageNumber, int pageSize) {
         return chapterDAO.getAllChaptersPaged(pageNumber, pageSize);
     }
 
+    public boolean doesChapterExistByBookNameAndChapterName(String bookName,String chapterName, String createdBy) {
+        return chapterDAO.doesChapterExistByBookNameAndChapterName(bookName, chapterName, createdBy);
+    }
+
     public List<Chapter> getChaptersByBookNamePaged(
-        String bookName,
-        int pageNumber,
-        int pageSize,
-        String createdBy
+            String bookName,
+            int pageNumber,
+            int pageSize,
+            String createdBy
     ) throws BookDoesNotExistException {
         return chapterDAO.getChaptersByBookNamePaged(bookName, pageNumber, pageSize, createdBy);
     }
 
+    public Chapter getChapterByBookNameAndChapterName(String bookName, String chapterName, String createdBy) {
+        Chapter chapter = chapterDAO.getChapterByBookNameAndChapterName(bookName, chapterName, createdBy);
+        return chapter;
+    }
+
     public Chapter getChapterByBookNameAndChapterNumber(
-        String bookName,
-        Long chapterNumber,
-        String createdBy
+            String bookName,
+            Long chapterNumber,
+            String createdBy
     ) throws BookDoesNotExistException, ChapterDoesNotExistException {
         return chapterDAO.getChapterByBookNameAndChapterNumber(
-            bookName,
-            chapterNumber,
-            createdBy
+                bookName,
+                chapterNumber,
+                createdBy
         );
     }
 
+    public Chapter updateChapterLockStatus(
+            Long bookNumber, Long chapterNumber, LockStatus lockStatus
+    ) {
+        Chapter chapter = chapterDAO.updateChapterLockStatusByBookNumberAndChapterNumber(
+                bookNumber, chapterNumber, lockStatus
+        );
+        return chapter;
+    }
+
+    public Chapter createChapterNoViews(
+            String bookName,
+            Long chapterNumber,
+            String chapterName,
+            String createdBy
+    ) {
+        Chapter chapter = createChapter(
+                bookName,
+                chapterNumber,
+                chapterName,
+                NO_VIEWS,
+                createdBy
+        );
+        return chapter;
+    }
+
     public Chapter createChapter(
-        String bookName,
-        Long chapterNumber,
-        String chapterName,
-        Long chapterViews,
-        String createdBy
+            String bookName,
+            Long chapterNumber,
+            String chapterName,
+            Long chapterViews,
+            String createdBy
     ) throws BookDoesNotExistException, ChapterAlreadyExistsException {
+        log.info("bookName: " + bookName);
+        log.info("chapterName: " + chapterName);
+        log.info("chapterNumber: " + chapterNumber);
+        log.info("chapterViews: " + chapterViews);
+        log.info("createdBy: " + createdBy);
         Book book = bookDAO.getBookByBookName(bookName, createdBy);
+        Long createdEpochMilli = timeService.getCurrentLocalDateTimeLong();
         return chapterDAO.createChapter(
-            book, chapterNumber, chapterName, chapterViews, createdBy
+                book, chapterNumber, chapterName, chapterViews, createdBy, createdEpochMilli,
+                LockStatus.EDITABLE
         );
     }
 
