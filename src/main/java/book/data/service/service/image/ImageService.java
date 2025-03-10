604b2824cd41cf5f213527d3d44b428fd36bca65
Matthew Stibbins <matthewstibbins@gmail.com>
2025-03-09 21:24:23 -0700

    add ImageService

---
+++ src/main/java/book/data/service/service/image/ImageService.java
@@ -0,0 +1,33 @@
+package book.data.service.service.image;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+
+@Slf4j
+@Service
+public class ImageService {
+    private ResourceLoader resourceLoader;
+
+    @Autowired
+    public ImageService(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+
+    public ImageData getImageData(String filePath) throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:" + filePath);
+        byte[] imageBytes =  Files.readAllBytes(resource.getFile().toPath());
+        String fileType = resource.getFile().getName();
+        fileType = fileType.substring(fileType.lastIndexOf(".")+1);
+        ImageData imageData = ImageData.builder()
+                .imageBytes(imageBytes)
+                .imageFileType(fileType)
+                .build();
+        return imageData;
+    }
+}
