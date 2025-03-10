a276b5384f161b04b6eb54428f92b6b39e80eaed
matthew stibbins <matthewstibbins@gmail.com>
2025-03-09 23:33:17 -0400

    Add lock status updates and new chapter methods
    Introduced methods to update chapter lock status and retrieve chapters by name. Extended `createChapter` to include lock status and creation timestamp. Improved logging for better debugging during chapter creation.

--- src/main/java/book/data/service/dao/chapter/ChapterDAO.java
+++ src/main/java/book/data/service/dao/chapter/ChapterDAO.java
@@ -2,12 +2,12 @@ package book.data.service.dao.chapter;
 
 import book.data.service.exception.BookDoesNotExistException;
 import book.data.service.exception.ChapterAlreadyExistsException;
-import book.data.service.exception.ChapterDoesNotExistException;
 import book.data.service.repository.BookRepository;
 import book.data.service.repository.ChapterRepository;
 import book.data.service.sqlmodel.book.Book;
 import book.data.service.sqlmodel.chapter.Chapter;
 import book.data.service.sqlmodel.chapter.ChapterId;
+import book.data.service.sqlmodel.chapter.LockStatus;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
@@ -22,8 +22,8 @@ public class ChapterDAO {
 
     @Autowired
     public ChapterDAO(
-        ChapterRepository chapterRepository,
-        BookRepository bookRepository
+            ChapterRepository chapterRepository,
+            BookRepository bookRepository
     ) {
         this.chapterRepository = chapterRepository;
         this.bookRepository = bookRepository;
@@ -42,10 +42,18 @@ public class ChapterDAO {
         return chapterRepository.findChaptersByBookNamePaged(bookName, pageRequest);
     }
 
+    public boolean doesChapterExistByBookNameAndChapterName(String bookName, String chapterName, String createdBy) {
+        return chapterRepository.doesChapterExistByBookNameAndChapterName(bookName, chapterName, createdBy);
+    }
+
+    public Chapter getChapterByBookNameAndChapterName(String bookName, String chapterName, String createdBy) {
+        return chapterRepository.findChapterByBookNameAndChapterName(bookName, chapterName, createdBy);
+    }
+
     public Chapter getChapterByBookNameAndChapterNumber(
-        String bookName,
-        Long chapterNumber,
-        String createdBy
+            String bookName,
+            Long chapterNumber,
+            String createdBy
     ) {
         if (!bookRepository.doesBookExist(bookName, createdBy)) {
             throw new BookDoesNotExistException();
@@ -56,24 +64,37 @@ public class ChapterDAO {
         return chapterRepository.findChapterByBookNameAndChapterNumber(bookName, chapterNumber, createdBy);
     }
 
+    public Chapter updateChapterLockStatusByBookNumberAndChapterNumber(Long bookNumber, Long chapterNumber, LockStatus lockStatus) {
+        chapterRepository.updateLockStatus(bookNumber, chapterNumber, lockStatus);
+        Chapter chapter = chapterRepository.findChapterByOnlyBookNumberAndChapterNumber(bookNumber, chapterNumber);
+        return chapter;
+    }
+
     public Chapter createChapter(
-        Book book,
-        Long chapterNumber,
-        String chapterName,
-        Long chapterViews,
-        String createdBy
+            Book book,
+            Long chapterNumber,
+            String chapterName,
+            Long chapterViews,
+            String createdBy,
+            Long createdEpochMilli,
+            LockStatus lockStatus
     ) {
         if (!bookRepository.doesBookExist(book.getBookName(), createdBy))  {
             throw new BookDoesNotExistException();
         }
         if (chapterRepository.doesChapterExist(book.getBookName(), chapterNumber, createdBy)) {
+            log.info("Book: " + book.toString());
+            log.info("Chapter Number: " + chapterNumber.toString());
+            log.info("chapterName : " + chapterName);
+            log.info("createdBy: " + createdBy.toString());
+            log.info("createdEpochMilli: " + createdEpochMilli.toString());
             throw new ChapterAlreadyExistsException();
         }
         ChapterId chapterId = new ChapterId(
-            chapterNumber,
-            book
+                chapterNumber,
+                book
         );
-        Chapter chapter = new Chapter(chapterId, chapterName, chapterViews, createdBy);
+        Chapter chapter = new Chapter(chapterId, chapterName, chapterViews, createdBy, createdEpochMilli, lockStatus);
         chapterRepository.saveChapter(chapter);
         return chapter;
     }
