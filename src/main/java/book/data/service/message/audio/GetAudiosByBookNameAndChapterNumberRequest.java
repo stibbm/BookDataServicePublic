cdaf70affc6084520b661e2d1747b58013c72946
Matthew Stibbins <matthewstibbins@gmail.com>
2025-03-10 01:39:59 -0700

    remove extra semicolon

--- src/main/java/book/data/service/message/audio/GetAudiosByBookNameAndChapterNumberRequest.java
+++ src/main/java/book/data/service/message/audio/GetAudiosByBookNameAndChapterNumberRequest.java
@@ -1,6 +1,7 @@
 package book.data.service.message.audio;
 
 import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,5 +20,5 @@ public class GetAudiosByBookNameAndChapterNumberRequest implements Serializable 
         Validate.notEmpty(bookName, "book name is required");
         Validate.notEmpty(chapterNumber, "chapterNumber is required");
         Long.parseLong(chapterNumber);
-        ;    }
+    }
 }
