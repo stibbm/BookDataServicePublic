62f94a9ec1ffd642cbcb8cef0863e24b9811082a
matthew stibbins <matthewstibbins@gmail.com>
2025-03-09 23:38:27 -0400

    Add new book retrieval and updating methods in BookDAO
    Introduce several methods to retrieve books by various criteria, such as creation time, views, name, and tags. Enhance the `createBook` method to include additional metadata and extend book update functionalities to handle updates by book number.

--- src/main/java/book/data/service/dao/book/BookDAO.java
+++ src/main/java/book/data/service/dao/book/BookDAO.java
@@ -1,17 +1,17 @@
 package book.data.service.dao.book;
 
-import book.data.service.exception.BookAlreadyExistsException;
-import book.data.service.exception.BookDoesNotExistException;
 import java.util.List;
-
-import book.data.service.repository.BookRepository;
 import java.util.Set;
 
+import book.data.service.exception.BookAlreadyExistsException;
+import book.data.service.exception.BookDoesNotExistException;
+import book.data.service.repository.BookRepository;
 import book.data.service.sqlmodel.book.Book;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
+
 
 @Slf4j
 @Component
@@ -29,33 +29,82 @@ public class BookDAO {
         return bookRepository.findAllBooksPaged(pageRequest, createdBy);
     }
 
+    public List<Book> getAllBooksSortedByBookViewsPaged(int pageNumber, int pageSize,
+                                                        String createdBy) {
+        PageRequest pageRequest = PageRequest.of(pageNumber, pageSize);
+        return bookRepository.findAllBooksSortedByBookViewsPaged(pageRequest, createdBy);
+    }
+
+    public List<Book> getAllBooksSortedByCreationTimePaged(int pageNumber, int pageSize,
+                                                           String createdBy) {
+        PageRequest pageRequest = PageRequest.of(pageNumber, pageSize);
+        return bookRepository.findAllBooksSortedByCreationTimePaged(pageRequest, createdBy);
+    }
+
+    public List<Book> getAllBooksSortedByBookNamePaged(int pageNumber, int pageSize, String createdBy) {
+        PageRequest pageRequest = PageRequest.of(pageNumber, pageSize);
+        return bookRepository.findAllBooksSortedByBookNamePaged(pageRequest, createdBy);
+    }
+
+    public List<Book> getBooksByBookTagPaged(String bookTag, int pageNumber, int pageSize, String createdBy) {
+        PageRequest pageRequest = PageRequest.of(pageNumber, pageSize);
+        return bookRepository.findBooksByBookTagPaged(bookTag, pageRequest, createdBy);
+    }
+
     public Book getBookByBookName(String bookName, String createdBy) {
+        log.info("bookName: " + bookName);
+        log.info("createdBy: " + createdBy);
         if (!bookRepository.doesBookExist(bookName, createdBy)) {
             throw new BookDoesNotExistException();
         }
         return bookRepository.findBookByBookName(bookName, createdBy);
     }
 
+    public Book getBookByBookNumber(Long bookNumber, String createdBy) {
+        if (!bookRepository.doesBookExistWithBookNumber(bookNumber, createdBy)) {
+            throw new BookDoesNotExistException();
+        }
+        return bookRepository.findBookByBookNumber(bookNumber, createdBy);
+    }
+
+    public boolean doesBookExistByBookNameOnly(String bookName) {
+        return this.bookRepository.doesBookExistWithBookNameOnly(bookName);
+    }
+
+    public Book getBookByBookNumberOnly(Long bookNumber) {
+        return this.bookRepository.findBookByBookNumberOnly(bookNumber);
+    }
+
+    public boolean doesBookExist(String bookName, String createdBy) {
+        return this.bookRepository.doesBookExist(bookName, createdBy);
+    }
+
     public Book createBook(
-        String bookName,
-        String createdBy,
-        String bookDescription,
-        String bookLanguage,
-        Long bookViews,
-        String bookThumbnail,
-        Set<String> bookTags
+            String bookName,
+            String createdBy,
+            String bookDescription,
+            String bookLanguage,
+            Long bookViews,
+            String bookThumbnail,
+            String bookThumbnailS3Key,
+            String bookThumbnailS3Bucket,
+            Set<String> bookTags,
+            Long createdAtEpochMilli
     ) {
         if (bookRepository.doesBookExist(bookName, createdBy)) {
             throw new BookAlreadyExistsException();
         }
         Book book = Book.createBookWithoutBookId(
-            bookName,
-            createdBy,
-            bookDescription,
-            bookLanguage,
-            bookViews,
-            bookThumbnail,
-            bookTags
+                bookName,
+                createdBy,
+                bookDescription,
+                bookLanguage,
+                bookViews,
+                bookThumbnail,
+                bookThumbnailS3Key,
+                bookThumbnailS3Bucket,
+                bookTags,
+                createdAtEpochMilli
         );
         bookRepository.saveBook(book);
         return book;
@@ -75,19 +124,36 @@ public class BookDAO {
             throw new BookDoesNotExistException();
         }
         Book book = bookRepository.findBookByBookName(bookName, createdBy);
-        bookRepository.deleteBook(bookName);
+        bookRepository.deleteBook(bookName, createdBy);
         return book;
     }
 
     public Book updateBookViews(
-        String bookName,
-        Long bookViews,
-        String createdBy
+            String bookName,
+            Long bookViews,
+            String createdBy
     ) {
         if (!bookRepository.doesBookExist(bookName, createdBy)) {
             throw new BookDoesNotExistException();
         }
-        bookRepository.updateBookViews(bookName, bookViews);
+        bookRepository.updateBookViews(bookName, bookViews, createdBy);
         return bookRepository.findBookByBookName(bookName, createdBy);
+    }
+
+    public Book getBookByBookNameOnly(String bookName) {
+        List<Book> bookList = bookRepository.findBooksByBookNameOnly(bookName);
+        if(bookList.size() > 1) {
+            log.error("More than 1 book found for the given name, am just picking one of them");
+        }
+        return bookList.get(0);
+    }
+
+    public Book updateBookViewsByBookNumber(Long bookNumber, Long updatedBookViews, String createdBy) {
+        if (!bookRepository.doesBookExistWithBookNumber(bookNumber, createdBy)) {
+            throw new BookDoesNotExistException();
+        }
+        bookRepository.updateBookViewsByBookNumber(bookNumber, updatedBookViews, createdBy);
+        Book updatedBook = bookRepository.findBookByBookNumberOnly(bookNumber);
+        return updatedBook;
     }
 }
