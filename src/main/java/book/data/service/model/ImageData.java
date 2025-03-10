08c6a5ac1198e6b8d46bb38a4ae65a3f85dfb190
Matthew Stibbins <48364517+stibbm@users.noreply.github.com>
2025-03-10 01:20:42 -0700

    add ImageData

---
+++ src/main/java/book/data/service/model/ImageData.java
@@ -0,0 +1,16 @@
+package book.data.service.model;
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ImageData implements Serializable {
+    private byte[] imageBytes;
+    private String imageFileType;
+}
