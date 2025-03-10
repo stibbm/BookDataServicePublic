e79ec3c9f2d1eaf29ad836643b1ed736af7bba6d
Matthew Stibbins <48364517+stibbm@users.noreply.github.com>
2025-03-10 01:23:34 -0700

    add VideoCreationMilestone enum

---
+++ src/main/java/book/data/service/model/VideoCreationMilestone.java
@@ -0,0 +1,11 @@
+package book.data.service.model;
+
+public enum VideoCreationMilestone {
+    STARTED,
+    TEXT_TRANSLATED,
+    AUDIO_CREATED,
+    VIDEO_CREATED,
+    VIDEO_UPLOADED_TO_YOUTUBE,
+    ADDED_TO_PLAYLIST,
+    COMPLETED
+}
