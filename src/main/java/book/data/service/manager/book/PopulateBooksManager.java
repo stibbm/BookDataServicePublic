d9dfa19e069ad84fd012f8382d3806b7620a2c42
Matthew Stibbins <matthewstibbins@gmail.com>
2025-03-11 12:44:10 -0700

    add PopulateBooksManager

---
+++ src/main/java/book/data/service/manager/book/PopulateBooksManager.java
@@ -0,0 +1,64 @@
+package book.data.service.manager.book;
+
+import book.data.service.sqlmodel.book.Book;
+import book.data.service.sqlmodel.youtubevideo.YoutubeVideoDetailsTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Slf4j
+@Service
+public class PopulateBooksManager {
+
+    private BookManager bookManager;
+    private VideoDetailsTemplates videoDetailsTemplates;
+
+    @Autowired
+    public PopulateBooksManager(BookManager bookManager,
+                                VideoDetailsTemplates videoDetailsTemplates) {
+        this.bookManager = bookManager;
+        this.videoDetailsTemplates = videoDetailsTemplates;
+    }
+
+    public List<Book> populateBooks(String createdBy)
+            throws MalformedURLException, URISyntaxException {
+        List<Series> seriesList = SeriesHelper.getSeriesList();
+        List<Book> bookList = new ArrayList<>();
+        for (Series series : seriesList) {
+            String seriesString = series.toString();
+            Map<String, PlayListData> playListMapBySeriesString = YoutubePlayLists.getPlayListMapBySeriesString();
+            YoutubeVideoDetailsTemplate youtubeVideoDetailsTemplate = videoDetailsTemplates.getSeriesTemplateAsYoutubeVideoDetailsTemplate(
+                    series);
+            Map<String, VideoDetailsTemplate> videoDetailsTemplateMap = new HashMap<>();
+
+            String bookName = seriesString.replace("_", " ");
+            Set<String> tagSet = youtubeVideoDetailsTemplate.getVideoTags();
+            String language = "Korean";
+            VideoDetailsTemplate videoDetailsTemplate = videoDetailsTemplateMap.get(seriesString);
+            boolean doesBookExist = bookManager.doesBookExistByBookNameOnly(bookName);
+            if (!doesBookExist) {
+                Book book = bookManager.createBookNoViews(
+                        bookName,
+                        createdBy,
+                        bookName,
+                        language,
+                        tagSet,
+                        YoutubePlayLists.getPlayListMapBySeriesString().get(seriesString)
+                                .getThumbnailBytes(),
+                        YoutubePlayLists.getPlayListMapBySeriesString().get(seriesString)
+                                .getThumbnailFileType()
+                );
+                bookList.add(book);
+            }
+        }
+        return bookList;
+    }
+}
