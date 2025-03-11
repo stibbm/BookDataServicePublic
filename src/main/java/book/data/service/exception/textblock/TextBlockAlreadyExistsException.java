fd0de086c9141b4985f88dd58f7da6881858970e
Matthew Stibbins <matthewstibbins@gmail.com>
2025-03-11 15:35:52 -0700

    add TextBlockAlreadyExistsException

---
+++ src/main/java/book/data/service/exception/textblock/TextBlockAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package book.data.service.exception.textblock;
+
+public class TextBlockAlreadyExistsException extends RuntimeException {
+    public TextBlockAlreadyExistsException(String message) {
+        super(message);
+    }
+}
