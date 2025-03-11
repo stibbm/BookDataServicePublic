32a5cf79e6ddc35390bb3373202e0cbc9595a691
Matthew Stibbins <matthewstibbins@gmail.com>
2025-03-11 15:33:31 -0700

    add CheckoutSessionDoesNotExistException

---
+++ src/main/java/book/data/service/exception/checkoutsession/CheckoutSessionDoesNotExistException.java
@@ -0,0 +1,7 @@
+package book.data.service.exception.checkoutsession;
+
+public class CheckoutSessionDoesNotExistException extends RuntimeException {
+    public CheckoutSessionDoesNotExistException(String message) {
+        super(message);
+    }
+}
