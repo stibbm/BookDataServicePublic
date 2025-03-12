93b400fdc8715e6f2ca61a3967b0b76687175868
matthew stibbins <matthewstibbins@gmail.com>
2025-03-12 08:55:07 -0700

    add PopulateYoutubeVideoDataByVideoIdActivity

--- src/main/java/book/data/service/activity/populate/youtube/PopulateYoutubeVideoDataByVideoIdActivity.java
+++ src/main/java/book/data/service/activity/populate/youtube/PopulateYoutubeVideoDataByVideoIdActivity.java
@@ -1,16 +1,16 @@
 package book.data.service.activity.populate.youtube;
 
-import static matt.book.data.service.constants.Routes.ALL_ORIGINS;
-import static matt.book.data.service.constants.Routes.POPULATE_YOUTUBE_VIDEO_DATA_BY_PLAYLIST_ID_AND_BOOK_NUMBER;
+import static book.data.service.constants.Routes.ALL_ORIGINS;
+import static book.data.service.constants.Routes.POPULATE_YOUTUBE_VIDEO_DATA_BY_PLAYLIST_ID_AND_BOOK_NUMBER;
 
+import book.data.service.firebase.FirebaseService;
+import book.data.service.manager.youtubevideo.YoutubeVideosPopulateDataForPlaylistManager;
+import book.data.service.message.youtube.PopulateYoutubeVideoDataByPlaylistIdAndBookNumberRequest;
+import book.data.service.message.youtube.PopulateYoutubeVideoDataByPlaylistIdAndBookNumberResponse;
+import book.data.service.sqlmodel.youtubevideo.YouTubeVideo;
 import java.io.IOException;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-import matt.book.data.service.firebase.FirebaseService;
-import matt.book.data.service.manager.youtubevideo.YoutubeVideosPopulateDataForPlaylistManager;
-import matt.book.data.service.messages.youtube.PopulateYoutubeVideoDataByPlaylistIdAndBookNumberRequest;
-import matt.book.data.service.messages.youtube.PopulateYoutubeVideoDataByPlaylistIdAndBookNumberResponse;
-import matt.book.data.service.sqlmodel.youtubevideo.YouTubeVideo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
